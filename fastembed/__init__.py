e4c3fae65d876dbd2df0796d9ca08011493577e5
Nathan LeRoy <nleroy917@users.noreply.github.com>
2026-01-20 12:46:55 -0500

    remove old modules and tests

--- fastembed/__init__.py
+++ fastembed/__init__.py
@@ -1,24 +1,13 @@
 import importlib.metadata
 
 from fastembed.bio import ProteinEmbedding
-from fastembed.image import ImageEmbedding
-from fastembed.late_interaction import LateInteractionTextEmbedding
-from fastembed.late_interaction_multimodal import LateInteractionMultimodalEmbedding
-from fastembed.sparse import SparseEmbedding, SparseTextEmbedding
-from fastembed.text import TextEmbedding
 
 try:
-    version = importlib.metadata.version("fastembed")
-except importlib.metadata.PackageNotFoundError as _:
-    version = importlib.metadata.version("fastembed-gpu")
+    version = importlib.metadata.version("fastembed-bio")
+except importlib.metadata.PackageNotFoundError:
+    version = "0.0.0"
 
 __version__ = version
 __all__ = [
-    "TextEmbedding",
-    "SparseTextEmbedding",
-    "SparseEmbedding",
-    "ImageEmbedding",
-    "LateInteractionTextEmbedding",
-    "LateInteractionMultimodalEmbedding",
     "ProteinEmbedding",
 ]
--- tests/config.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-TEST_DIR = Path(__file__).parent
-TEST_MISC_DIR = TEST_DIR / "misc"
